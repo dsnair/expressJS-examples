39042a9e7234f605322a212a1119d593fc485f94
Divya Nair <divnairs@gmail.com>
2019-05-07 16:59:54 -0700

    protect /user with middleware

--- auth-session/index.js
+++ auth-session/index.js
@@ -58,12 +58,10 @@ const login = async (req, res) => {
     }
   } catch (error) {
     console.error(error)
-    res
-      .status(500)
-      .json({
-        error,
-        msg: `Something went wrong while logging-in ${req.body.username}.`
-      })
+    res.status(500).json({
+      error,
+      msg: `Something went wrong while logging-in ${req.body.username}.`
+    })
   }
 }
 
@@ -80,9 +78,37 @@ const getUsers = async (req, res) => {
 }
 
 // middleware
+const protectRoute = async (req, res, next) => {
+  const { username, password } = req.headers
+
+  if (!username || !password)
+    return res.status(400).send(`Unauthorized user. Please login first.`)
+
+  try {
+    const user = await knex('users')
+      .where('username', username)
+      .first()
+
+    if (!user) return res.status(400).send(`${username} doesn't exist.`)
+    else {
+      const isAuthenticated = await bcrypt.compareSync(password, user.password)
+      isAuthenticated
+        ? next()
+        : res
+            .status(401)
+            .send(`Uh, oh! Either the username or password is incorrect.`)
+    }
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      error,
+      msg: `Something went wrong while logging-in ${username}.`
+    })
+  }
+}
 
 // routes
 app.get('/', (rep, res) => res.send('server is alive 🥁'))
 app.post('/register', register)
 app.post('/login', login)
-app.get('/users', getUsers)
+app.get('/users', protectRoute, getUsers)

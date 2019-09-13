b9fca00cc890955061165d7d0419fd11a46117c5
Divya Nair <divnairs@gmail.com>
2019-09-13 16:02:08 -0700

    API design basic: query params

--- basic/index.js
+++ basic/index.js
@@ -14,12 +14,27 @@ let db = [
   {
     id: 2,
     pet: 'cat'
+  },
+  {
+    id: 3,
+    pet: 'fish'
+  },
+  {
+    id: 4,
+    pet: 'hamster'
   }
 ]
 
 // route handlers
 const get = (req, res) => {
-  res.status(200).send(db)
+  res.status(200).send(
+    db
+      .slice(0, req.query.limit) // pagination
+      .sort((a, b) => {
+        const sortby = req.query.sortby in a ? req.query.sortby : 'id'
+        return a[sortby] > b[sortby] ? 1 : -1
+      }) // sort
+  )
 }
 
 const post = (req, res) => {
